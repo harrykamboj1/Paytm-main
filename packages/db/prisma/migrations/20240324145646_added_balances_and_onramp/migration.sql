31cb90f7a07940c90b833b422673b3565da212c7
Harnoor singh <singhharnoor116@gmail.com>
2024-04-14 11:25:55 +0530

    New changes

--- packages/db/prisma/migrations/20240324145646_added_balances_and_onramp/migration.sql
+++ packages/db/prisma/migrations/20240324145646_added_balances_and_onramp/migration.sql
@@ -2,16 +2,6 @@
 CREATE TYPE "OnRampStatus" AS ENUM ('Success', 'Failure', 'Processing');
 
 -- CreateTable
-CREATE TABLE "Balance" (
-    "id" SERIAL NOT NULL,
-    "amount" INTEGER NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "locked" INTEGER NOT NULL,
-
-    CONSTRAINT "Balance_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "OnRampTransaction" (
     "id" SERIAL NOT NULL,
     "status" "OnRampStatus" NOT NULL,
@@ -24,14 +14,24 @@ CREATE TABLE "OnRampTransaction" (
     CONSTRAINT "OnRampTransaction_pkey" PRIMARY KEY ("id")
 );
 
--- CreateIndex
-CREATE UNIQUE INDEX "Balance_userId_key" ON "Balance"("userId");
+-- CreateTable
+CREATE TABLE "Balance" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "locked" INTEGER NOT NULL,
+
+    CONSTRAINT "Balance_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateIndex
 CREATE UNIQUE INDEX "OnRampTransaction_token_key" ON "OnRampTransaction"("token");
 
--- AddForeignKey
-ALTER TABLE "Balance" ADD CONSTRAINT "Balance_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+-- CreateIndex
+CREATE UNIQUE INDEX "Balance_userId_key" ON "Balance"("userId");
 
 -- AddForeignKey
 ALTER TABLE "OnRampTransaction" ADD CONSTRAINT "OnRampTransaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Balance" ADD CONSTRAINT "Balance_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
